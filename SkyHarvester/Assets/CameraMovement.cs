d8c2e0a38032aada015d3c1479f7e7125be7de78
Atte Moisio <atte.moisio@attemoisio.fi>
2013-09-29 20:01:35 +0300

    Add Unity project

---
+++ SkyHarvester/Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform camera;
+    public Transform target;
+ 
+    void Update () 
+    {
+        camera.position = target.position + new Vector3(0, 10, -470);
+    }
+
+}
---
+++ SkyHarvester/Assets/Puimuri/CombineControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombineControl : MonoBehaviour {
+
+    public WheelCollider rearWheel;
+    public WheelCollider frontWheel;
+    public Transform frontWheelTransform;
+    public Transform rearWheelTransform;
+
+	public float maxTorque = 50F;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rearWheel.motorTorque = -maxTorque;
+            frontWheel.motorTorque = -maxTorque;
+        }
+		else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rearWheel.motorTorque = maxTorque;
+            frontWheel.motorTorque = maxTorque;
+        }
+		else {
+			rearWheel.motorTorque = 0;
+			frontWheel.motorTorque = 0;
+		}
+		
+	}
+
+    void Update()
+    {
+        frontWheelTransform.Rotate( 0, 0, -frontWheel.rpm / 60F * 360F * Time.deltaTime);
+        rearWheelTransform.Rotate( 0, 0, -rearWheel.rpm / 60F * 360F * Time.deltaTime);
+    }
+}
